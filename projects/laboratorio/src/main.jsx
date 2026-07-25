cd881cae6a61ed7baeeedde250a514d95c2b7e13
AleSC203 <ale1010192@gmail.com>
2026-07-25 14:19:17 -0600

    Base y DB

---
+++ projects/laboratorio/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div>
+      <h1>Hola Mundo</h1>
+      <button>Probando</button>
+    </div>
+  </StrictMode>,
+)
